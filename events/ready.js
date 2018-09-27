b240e785803e7146df87d1e266b4da192fde1f83
InkoHX <inkohx@gmail.com>
2018-09-27 19:53:13 +0900

    add ready event

---
+++ events/ready.js
@@ -0,0 +1,16 @@
+const { Event } = require("klasa")
+
+module.exports = class extends Event {
+
+  constructor(...args) {
+    super(...args, {
+      enabled: true,
+      once: false,
+      event: "ready"
+    })
+  }
+
+  async run() {
+    await this.client.user.setActivity(`@${this.client.user.tag} help｜djs-jpn.ga`)
+  }
+}
