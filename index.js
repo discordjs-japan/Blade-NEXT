c1c0076a5e76df77055ca791c013a998375ad003
InkoHX <inkohx@gmail.com>
2018-09-27 19:55:10 +0900

    fix

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ class BladeNEXTClient extends Client {
 
   constructor(...args) {
     super(...args)
-
-    // Add any properties to your Klasa Client
   }
-
-  // Add any methods to your Klasa Client
-
 }
 
 new BladeNEXTClient(config).login(token)
