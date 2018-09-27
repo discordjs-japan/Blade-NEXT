4a963b9cc84c716f419b879a893ef2ed77315ec9
InkoHX <inkohx@gmail.com>
2018-09-27 19:53:24 +0900

    add avatar command

---
+++ commands/General/User/avatar.js
@@ -0,0 +1,35 @@
+const { Command } = require("klasa")
+
+module.exports = class extends Command {
+
+  constructor(...args) {
+    /**
+         * Any default options can be omitted completely.
+         * if all options are default, you can omit the constructor completely
+         */
+    super(...args, {
+      enabled: true,
+      runIn: ["text", "dm", "group"],
+      requiredPermissions: [],
+      requiredSettings: [],
+      aliases: [],
+      autoAliases: true,
+      bucket: 1,
+      cooldown: 0,
+      promptLimit: 0,
+      promptTime: 30000,
+      deletable: false,
+      guarded: false,
+      nsfw: false,
+      permissionLevel: 0,
+      description: language => language.get("COMMAND_AVATAR_DESCRIPTION"),
+      usageDelim: undefined,
+      quotedStringSupport: false,
+      subcommands: false
+    })
+  }
+
+  async run(message) {
+    return message.sendMessage(message.author.avatarURL())
+  }
+}
