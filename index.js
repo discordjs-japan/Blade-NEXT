1bfadac3d2d4917e1480cf4407d580b24bb1b3f5
InkoHX <inkohx@gmail.com>
2018-09-29 19:22:46 +0900

    Add BladeNEXT API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client } = require("klasa")
 const { config, token } = require("./config")
 
+Client.use(require("klasa-dashboard-hooks"))
 class BladeNEXTClient extends Client {
 
   constructor(...args) {
