06aef193107c10dc0c7dc4ee9aafd980c37083a9
InkoHX <inkohx@gmail.com>
2018-10-01 20:31:11 +0900

    Update Avatar command

--- commands/General/User/avatar.js
+++ commands/General/User/avatar.js
@@ -7,14 +7,17 @@ module.exports = class extends Command {
     super(...args, {
       runIn: ["text", "dm", "group"],
       description: language => language.get("COMMAND_AVATAR_DESCRIPTION"),
+      usage: "[user:user]",
+      requiredPermissions: ["EMBED_LINKS"]
     })
   }
 
-  async run(message) {
+  async run(message, [user = message.author]) {
+    if (!user) user = await message.guild.members.fetch(message.author.id).catch(() => null)
     return message.sendMessage(new MessageEmbed()
       .setTitle("Avatar Link")
-      .setURL(message.author.avatarURL() || "https://github.com/DJS-JPN/res/blob/master/No_Image.png?raw=true")
-      .setImage(message.author.avatarURL() || "https://github.com/DJS-JPN/res/blob/master/No_Image.png?raw=true")
+      .setURL(user.avatarURL() || "https://github.com/DJS-JPN/res/blob/master/No_Image.png?raw=true")
+      .setImage(user.avatarURL() || "https://github.com/DJS-JPN/res/blob/master/No_Image.png?raw=true")
     )
   }
 }
