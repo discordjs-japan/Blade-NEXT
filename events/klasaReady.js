7902f7fbf9d98b072074c6393454615e190b53ec
InkoHX <inkohx@gmail.com>
2018-10-22 16:23:14 +0900

    fix

--- events/klasaReady.js
+++ events/klasaReady.js
@@ -1,7 +1,5 @@
 const { Event } = require("klasa")
 
 module.exports = class extends Event {
-  async run() {
-    await this.client.user.setActivity(`@${this.client.user.tag} help｜djs-jpn.ga`, {type: "PLAYING"})
-  }
+  async run() {}
 }
--- tasks/UpdatePlayingGameTask.js
+++ tasks/UpdatePlayingGameTask.js
@@ -1,18 +1,23 @@
 const { Task } = require("klasa")
 
 module.exports = class extends Task {
+  constructor(...args) {
+    super(...args, {enadle: true}) 
+  }
+
   async run() {
-    const playing = [
+    await this.client.user.setActivity(this.playing[this.client.fun.random(0, 4)], {type: "PLAYING"})
+  }
+
+  async init() {
+    this.playing = [
       `@${this.client.user.tag} help｜djs-jpn.ga`,
       `${this.client.guilds.size} guilds｜djs-jpn.ga`,
       `Default Prefix：${this.client.options.prefix}｜djs-jpn.ga`,
       `${this.client.users.size} users｜djs-jpn.ga`,
       `${this.client.commands.size} commands｜djs-jpn.ga`
     ]
-    await this.client.user.setActivity(playing[this.client.fun.random(0, 4)], {type: "PLAYING"})
-  }
-
-  async init() {
-    this.client.schedule.create("UpdatePlayingGameTask", "* */3 * * *")
+    this.client.schedule.create("UpdatePlayingGameTask", "* */5 * * *")
+    await this.client.user.setActivity(this.playing[this.client.fun.random(0, 4)], {type: "PLAYING"})
   }
 }
