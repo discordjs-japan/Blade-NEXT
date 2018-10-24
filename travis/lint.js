4da4fa4f7d95cda090bb886fb4cd02dd140ee8c8
InkoHX <inkohx@gmail.com>
2018-10-25 02:36:31 +0900

    Update travis/lint.js

--- travis/lint.js
+++ travis/lint.js
@@ -7,6 +7,18 @@ const path = require('path')
 
 const report = cli.executeOnFiles(['.'])
 
+if (report.errorCount === 0 && report.warningCount === 0) {
+  const embed = {
+    description: ':o: Good coding',
+    color: 0X00FF00,
+    author: {
+      name: 'ESLint',
+      icon_url: 'http://eslint.org/img/favicon.512x512.png',
+    },
+  }
+  hook.send({embeds: [embed]}).then(() => {process.exit()})
+}
+
 const files = report.results.filter(e => e.source)
 const hasError = report.errorCount
 const all = `${report.errorCount + report.warningCount} problems`
